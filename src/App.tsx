5c2c1ba03acc2d3154b2b99b71c803e1a475a371
Kristina Taneva <tinatanev@gmail.com>
2019-11-09 21:37:49 +0100

    feat: implement route handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import './App.css';
-import TodoEntry from './components/TodoEntry/TodoEntry';
-import TodosPreview from './components/TodosPreview/TodosPreview';
+import React from "react";
+import AddOrEditTodo from "./components/AddOrEditTodo/AddOrEditTodo";
+import TodosPreview from "./components/TodosPreview/TodosPreview";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import TodoDetails from "./components/TodoDetails/TodoDetails";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/entry">
-          <TodoEntry></TodoEntry>
-        </Route>
-        <Route path="/">
-          <TodosPreview></TodosPreview>
-        </Route>
+        <Route path="/entry" render={props => <AddOrEditTodo {...props} />} />
+        <Route path="/edit/:id" render={props => <AddOrEditTodo {...props} editMode />} />
+        <Route path="/" component={TodosPreview} exact />
+        <Route path="/details/:id" component={TodoDetails} />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
